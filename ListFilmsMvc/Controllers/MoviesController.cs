bcd73305fb8e7c3eed59acb18e17d4a19ca65192
Luis Rachope <luis.chiasso@hotmail.com>
2022-11-02 14:13:16 -0300

    Primeira migration e dados colhidos do banco MySql

--- ListFilmsMvc/Controllers/MoviesController.cs
+++ ListFilmsMvc/Controllers/MoviesController.cs
@@ -11,29 +11,17 @@ namespace ListFilmsMvc.Controllers
 {
     public class MoviesController : Controller
     {
-        //private readonly ListFilmsMvcContext _context;
+        private readonly ListFilmsMvcContext _context;
 
-        //public MoviesController(ListFilmsMvcContext context)
-        //{
-        //_context = context;
-        //}
+        public MoviesController(ListFilmsMvcContext context)
+        {
+            _context = context;
+        }
 
 
         public IActionResult Index()
         {
-            List<Movie> movies = new List<Movie>();
-            movies.Add(new Movie(1, "O Poderoso Chefão", "Francis Ford Coppola", 1972, TypeGenre.Drama,
-                TypeCategory.Film, 9.2));
-            movies.Add(new Movie(2, "Guerra nas Estrelas", "George Lucas", 1977, TypeGenre.Adventure,
-                TypeCategory.Film, 8.6));
-            movies.Add(new Movie(3, "O Silêncio dos Inocentes", "Jonathan Demme", 1991, TypeGenre.Drama,
-                TypeCategory.Film, 8.6));
-            movies.Add(new Movie(4, "The Office", "Greg Daniels", 2005, TypeGenre.Comedy,
-                TypeCategory.Series, 9.0));
-            movies.Add(new Movie(5, "O Mandaloriano", "Jon Favreau", 2019, TypeGenre.Fantasy,
-                TypeCategory.Series, 8.7));
-            movies.Add(new Movie(6, "Alquimia das Almas", "Jonathan Demme", 2022, TypeGenre.Fantasy,
-                TypeCategory.Series, 8.7));
+            List<Movie> movies = _context.Movie.ToList();
 
             return View(movies);
         }
--- ListFilmsMvc/Data/SeedingService.cs
+++ ListFilmsMvc/Data/SeedingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ListFilmsMvc.Models;
+using ListFilmsMvc.Models.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +14,29 @@ namespace ListFilmsMvc.Data
         public SeedingService(ListFilmsMvcContext context)
         {
             _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Movie.Any())
+            {
+                return; // DB has been seeded
+            }
+
+            // Creation data to migration on DB (Movie)
+            Movie m1 = new Movie(1, "O Poderoso Chefão", "Francis Ford Coppola", 1972, TypeGenre.Drama, TypeCategory.Film, 9.2);
+            Movie m2 = new Movie(2, "Guerra nas Estrelas", "George Lucas", 1977, TypeGenre.Adventure, TypeCategory.Film, 8.6);
+            Movie m3 = new Movie(3, "O Silêncio dos Inocentes", "Jonathan Demme", 1991, TypeGenre.Drama, TypeCategory.Film, 8.6);
+            Movie m4 = new Movie(4, "The Office", "Greg Daniels", 2005, TypeGenre.Comedy, TypeCategory.Series, 9.0);
+            Movie m5 = new Movie(5, "O Mandaloriano", "Jon Favreau", 2019, TypeGenre.Fantasy, TypeCategory.Series, 8.7);
+            Movie m6 = new Movie(6, "Alquimia das Almas", "Jonathan Demme", 2022, TypeGenre.Fantasy, TypeCategory.Series, 8.7);
+
+            // Adding movies on DB
+            _context.Movie.AddRange(m1, m2, m3, m4, m5, m6);
+
+            // Execution saving data
+            _context.SaveChanges();
+
         }
     }
 }
